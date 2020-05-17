8de50bb3cc2f9bc421a6cabb463460bee2ddcab8
Aaron Watters <awatters@simonsfoundation.org>
2020-05-17 08:30:08 -0400

    use single quotes for embedding in bash scripts

--- jest/run_jupyter.py
+++ jest/run_jupyter.py
@@ -4,31 +4,31 @@
 import subprocess
 import os
 
-JUPYTER_URL_PATH = "./_jupyter_url.txt"
+JUPYTER_URL_PATH = './_jupyter_url.txt'
 
 def run():
-    cmd = ["jupyter", "notebook", "--port=3000", "--no-browser"]
-    print ("Starting jupyter server: " + repr(cmd))
+    cmd = ['jupyter', 'notebook', '--port=3000', '--no-browser']
+    print ('Starting jupyter server: ' + repr(cmd))
     proc = subprocess.Popen(cmd, stderr=subprocess.PIPE)
     url_emitted = False
     try:
         for line in proc.stderr:
-            print("Jupyter_server: " + repr(line))
-            line = str(line, encoding="utf8")
+            print('Jupyter_server: ' + repr(line))
+            line = str(line, encoding='utf8')
             if not url_emitted:
-                # assume first line starting "http:..." gives the start url with token similar to
+                # assume first line starting 'http:...' gives the start url with token similar to
                 # http://localhost:3000/?token=793337e53c6ca95680623cb6556afdb32c7a1ee002f60119
                 sline = line.strip()
-                if sline.startswith("http://"):
-                    with open(JUPYTER_URL_PATH, "w") as f:
+                if sline.startswith('http://'):
+                    with open(JUPYTER_URL_PATH, 'w') as f:
                         f.write(sline)
                     url_emitted = True
-                    print ("Jupyter url emitted: " + repr(sline))
-                    print ("   saved to: " + repr(JUPYTER_URL_PATH))
+                    print ('Jupyter url emitted: ' + repr(sline))
+                    print ('   saved to: ' + repr(JUPYTER_URL_PATH))
     finally:
-        print ("Stopping jupyter server", cmd)
+        print ('Stopping jupyter server', cmd)
         proc.kill()
         os.remove(JUPYTER_URL_PATH)
 
-if __name__=="__main__":
+if __name__=='__main__':
     run()
